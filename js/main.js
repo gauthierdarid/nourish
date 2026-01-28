aeee48dc18b18fde4165279febdb245fea2a9807
gauthierdarid <75815062+gauthierdarid@users.noreply.github.com>
2026-01-28 01:16:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+function setActiveNav() {
+  const path = location.pathname.split("/").pop() || "index.html";
+  document.querySelectorAll("[data-nav]").forEach(a => {
+    if (a.getAttribute("href").includes(path)) a.style.background = "rgba(255,255,255,.35)";
+  });
+}
 
+document.addEventListener("DOMContentLoaded", setActiveNav);
